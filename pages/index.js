737643e83a8059c9ea03e68cb01ddc7c16184a19
bleek42 <brandonleek42@gmail.com>
2021-10-17 11:14:11 -0400

    started crash course: created layout component

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Next.js Practice</title>
+        <meta name="keywords" content="react, next.js, web development, tutorial, node.js, javascript, jsx, server-side rendering, static-site generation" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Welcome to next</h1>
+      </main>
+    </div>
+  );
+}
